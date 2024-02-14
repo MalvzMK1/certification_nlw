c22ee3cb24e2b0ab7f6b4cf96c3ddfde2ec42484
Matheus Alves <matheus.adeoliveira11@gmail.com>
2024-02-14 13:35:19 -0300

    feat: add entities models

---
+++ src/main/java/com/skellington/certification_nlw/modules/certificates/entities/CertificateEntity.java
@@ -0,0 +1,11 @@
+package com.skellington.certification_nlw.modules.certificates.entities;
+
+import java.util.List;
+
+public class CertificateEntity {
+  private int id;
+  private int studentId;
+  private String tech;
+  private String grate;
+  private List<String> answersCertification;
+}
---
+++ src/main/java/com/skellington/certification_nlw/modules/students/entities/StudentEntity.java
@@ -0,0 +1,11 @@
+package com.skellington.certification_nlw.modules.students.entities;
+
+import java.util.List;
+import java.util.UUID;
+
+public class StudentEntity {
+  
+  private UUID id;
+  private String email;
+  private List<String> certificates;
+}
